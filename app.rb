52a6ccfdd51ff8dfcb935e33d4faf5f2f1b1cd0f
Omar Ansari <112027737+0marAnsari@users.noreply.github.com>
2024-10-26 01:15:28 +0000

    elephant

--- app.rb
+++ app.rb
@@ -6,4 +6,17 @@ get("/") do
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+  erb(:elephant)
+end
+
+get("/rock") do
+
+end
+
+get("/paper") do
+
+end
+
+get("/scissors") do
+
 end
