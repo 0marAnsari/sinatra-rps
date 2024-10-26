a845628b1df66b5752f1bf8d52e24785d51986fc
Omar Ansari <112027737+0marAnsari@users.noreply.github.com>
2024-10-26 04:19:48 +0000

    Finished.

--- app.rb
+++ app.rb
@@ -10,13 +10,58 @@ get("/") do
 end
 
 get("/rock") do
+  choice = "rock"
+  @random_choice = ["They played rock!", "They played paper!", "They played scissors!"].sample
+  outcome = ""
+  
+  if @random_choice == "They played rock!"
+    
+    @outcome = "We tied!"
 
+  elsif @random_choice == "They played paper!"
+    @outcome = "We lost!"
+
+  elsif @random_choice == "They played scissors!"
+    @outcome = "We won!"
+  end
+
+  erb(:rock)
 end
 
 get("/paper") do
+  choice = "paper"
+  @random_choice = ["They played rock!", "They played paper!", "They played scissors!"].sample
+  outcome = ""
+  
+  if @random_choice == "They played paper!"
+    
+    @outcome = "We tied!"
 
+  elsif @random_choice == "They played scissors!"
+    @outcome = "We lost!"
+
+  elsif @random_choice == "They played rock!"
+    @outcome = "We won!"
+  end
+
+  erb(:paper)
 end
 
 get("/scissors") do
+  choice = "scissors"
+  @random_choice = ["They played rock!", "They played paper!", "They played scissors!"].sample
+  outcome = ""
+  
+  if @random_choice == "They played scissors!"
+    
+    @outcome = "We tied!"
 
+  elsif @random_choice == "They played rock!"
+    @outcome = "We lost!"
+
+  elsif @random_choice == "They played paper!"
+    @outcome = "We won!"
+  end
+
+  erb(:scissors)
 end
